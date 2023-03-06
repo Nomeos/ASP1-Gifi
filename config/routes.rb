d7c708ba47091283e0b849323c6efbd75d67fe72
Nomeos <mathieu.rabot01@gmail.com>
2023-03-06 14:39:54 +0100

    default project

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # get 'semesters/index'
+  # get 'semesters/show'
+  # get 'semesters/edit'
+  # get 'semesters/new'
+  # get 'grades/index'
+  # get 'grades/show'
+  # get 'grades/edit'
+  # get 'grades/new'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "semesters#index"
+
+  resources :semesters
+  resources :grades
+end
