2207543004ec5c26e1b3f4cd90df325c45435437
Nomeos <mathieu.rabot01@gmail.com>
2023-03-06 16:06:18 +0100

    Create fake lectures

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,14 @@ require 'faker'
 5.times do
   Category.create!(name: Faker::Book.genre)
 end
+
+# Create lectures
+10.times do
+  Lecture.create!(
+    name: Faker::Educator.course_name,
+    description: Faker::Lorem.paragraph(sentence_count: 3),
+    start_date: Faker::Date.between(from: 1.month.ago, to: 1.month.from_now),
+    end_date: Faker::Date.between(from: 1.month.from_now, to: 3.months.from_now),
+    category_id: Category.all.sample.id
+  )
+end
