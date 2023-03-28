3ad682cd80c7ccd596c7dc7dd72ae24d28b71fb0
Nomeos <mathieu.rabot01@gmail.com>
2023-03-28 15:07:23 +0200

    remove the email from the devise seeder

--- db/migrate/20230320132118_add_devise_to_people.rb
+++ db/migrate/20230320132118_add_devise_to_people.rb
@@ -4,7 +4,7 @@ class AddDeviseToPeople < ActiveRecord::Migration[7.0]
   def self.up
     change_table :people do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      #t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
